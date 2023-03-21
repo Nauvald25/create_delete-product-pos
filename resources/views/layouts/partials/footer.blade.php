a89709647826b38361c912be5a24ed6819fdb6e6
Nauvald25 <mnauval390@gmail.com>
2023-03-22 00:03:55 +0700

    post

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Tahun 2023</b> 
+    </div>
+    <strong>Copyright &copy; 2023 <a href="http://github.com/Mnauvald25">Mnauvald25</a>.</strong> 
+  </footer>
